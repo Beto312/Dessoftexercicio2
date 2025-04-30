b384a60b8a6173962c015363f7addfde077bd77f
Beto312 <robertomb4@al.insper.edu.br>
2025-04-30 15:42:00 -0300

    funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,10 @@ def rolar_dados (numero):
         valor = random.randint(1, 6)
         dados.append(valor)
     return dados
+
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dado = dados_rolados[dado_para_guardar] # pega o valor do dado
+    dados_no_estoque.append(dado) # adiciona o dado na lista
+    del dados_rolados[dado_para_guardar] # remove o dado da lista de dados ja rolados
+    return [dados_rolados, dados_no_estoque] 
